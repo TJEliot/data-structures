6e27488c9114b6151bf0db150a8397de087c268e
Thomas <bgaesop@Thomass-MacBook.local>
2017-11-29 12:17:35 -0800

    made stacks work, and most of queues

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var counter = 0;
+  var lowest = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[counter] = value;
+    counter++;
   };
 
   someInstance.dequeue = function() {
+    var returner;
+    if (counter > 0){
+      returner = storage[0];
+      counter--;
+      delete storage[0];
+    }
+    return returner;
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
