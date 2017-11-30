527f9af847317d3c07598967d895b0218d826bc2
bgaesop <bgaesop@gmail.com>
2017-11-30 11:41:06 -0800

    Add linkedList and tree implementation

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,17 +1,38 @@
 var LinkedList = function() {
   var list = {};
+  var counter = 0;
+  var front = 0;
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    list['node' + counter] = new Node(value);
+    this.tail = list['node' + counter];  
+    if (this.head === null) {
+      this.head = list['node' + counter];
+    }
+    counter++;
   };
 
   list.removeHead = function() {
+    if (this.head !== null) {
+      var holder = this.head;
+      delete this.head;
+      delete list['node' + front];
+      front++;
+      this.head = list['node' + front];
+      return holder.value;
+    }
   };
 
   list.contains = function(target) {
+    for (var key in list) {
+      if (list[key].value === target) {
+        return true;
+      }
+    }
+    return false;
   };
-
   return list;
 };
 
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,29 @@
 var Tree = function(value) {
   var newTree = {};
-  newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree['value'] = value;
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
+  newTree['children'] = [];  
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this['children'].push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  if (this['value'] === target) {
+    return true;
+  }
+  for (var i = 0; i < this['children'].length; i++) {
+    if (this['children'][i].contains(target)){
+      return true;
+    }
+
+  } 
+  return false; 
 };
 
 
