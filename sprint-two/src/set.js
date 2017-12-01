256026d2c685f70b1d840440d48a82af1f610247
bgaesop <bgaesop@gmail.com>
2017-12-01 10:41:49 -0800

    implement set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,14 +7,24 @@ var Set = function() {
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this['_storage'][item] = item;
+  console.log(JSON.stringify(this['_storage']));
 };
 
 setPrototype.contains = function(item) {
+  if (this['_storage'][item] === item) {
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  delete this['_storage'][item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ add is constant
+ contains is constant
+ remove is constant
  */
