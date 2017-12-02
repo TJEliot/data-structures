2ba1642f95b940555fc6670bf130167217c27e9a
bgaesop <bgaesop@gmail.com>
2017-12-01 16:15:39 -0800

    implemented hashTable, including doubling in size

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,20 +1,76 @@
-
-
-var HashTable = function() {
-  this._limit = 8;
+var HashTable = function(size) {
+  if (size === undefined) {
+    size = 8;
+  }
+  this._limit = size;
   this._storage = LimitedArray(this._limit);
+  this.counter = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
+  this.counter++;
+  var currentHash = this;
+  var newHashMaker = function() {
+    var newHash = new HashTable(currentHash['_limit'] * 2);
+    var newFunc = newHash.insert(k, v);
+    currentHash['_storage'].each( function() {
+      var index = getIndexBelowMaxForKey(k, this['_limit']);
+      var bucket = newHash['_storage'].get(index);
+      if (newHash.retrieve(k)) {
+        newHash.remove(k);
+      }
+      if (bucket === undefined) {
+        bucket = [];
+      }
+      bucket.push([k, v]);
+      newHash['_storage'].set(index, bucket);
+    });
+    returner = [];
+    returner.push(newHash['_limit']);
+    returner.push(newHash['_storage']);
+    returner.push(newHash['counter']);
+    return returner;
+  };
+  if (this['counter'] >= .75 * this['_limit']) {
+    var holder = newHashMaker();
+    this['_limit'] = holder[0];
+    this['_storage'] = holder[1];
+    //console.log('before we make it bigger counter is ' + this['counter']);
+    this['counter'] = holder[2];
+    //console.log('we made it bigger and now counter is ' + this['counter']);
+  }    
+  //below this actually does the adding
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this['_storage'].get(index);
+  if (this.retrieve(k)) {
+    this.remove(k);
+  }
+  if (bucket === undefined) {
+    bucket = [];
+  }
+  bucket.push([k, v]);
+  this['_storage'].set(index, bucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this['_storage'].get(index);
+  if (bucket === undefined) {
+    return false;
+  }
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      return bucket[i][1];
+    }
+  }
 };
 
-HashTable.prototype.remove = function(k) {
+HashTable.prototype.remove = function(k) { 
   var index = getIndexBelowMaxForKey(k, this._limit);
+//  console.log(this['storage'].get(index));
+  this['_storage'].get(index).pop();
+  this.counter--;
+  
 };
 
 
